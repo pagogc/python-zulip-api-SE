9d245916527243e46a0359016be4babb0a87e9ff
pagogc <michael.pagler@gmx.at>
2024-01-16 09:18:23 +0100

    infobot

---
+++ zulip_bots/zulip_bots/bots/Info/Info.py
@@ -0,0 +1,50 @@
+# See readme.md for instructions on running this code.
+
+from typing import Any, Dict
+
+from zulip_bots.lib import BotHandler
+
+
+class HelloWorldHandler:
+    def usage(self) -> str:
+        return """
+        Simple Zulip bot that will respond to any query with a "not enough information" message.
+        """
+
+    def handle_message(self, message: Dict[str, Any], bot_handler: BotHandler) -> None:
+        content = """
+        :warning: :warning: :warning:  **Fehlende Informationen** :warning:  :warning:  :warning:  
+
+        **Dieser Thread enthält nicht alle notwendigen Informationen um eine Antwort zu geben.**
+        ---
+        Vorlage für einen Thread mit allen wichtigen Informationen:
+
+        Version: 7.00.403.xxxx
+        Oberfläche: WebUi oder WinUi
+        Datenbankformat: Classic oder Vectoring
+
+        Kurzbeschreibung: Tragen Sie hier eine kurze Info zu dem Problem ein
+        Beschreibung: Tragen Sie hier die komplette Beschreibung des Problems ein,
+        inkl. der Testszenarien die bereits durchgeführt wurden.
+        Bspw. ob das Problem auf einem anderen System geprüft wurde oder ob 
+        das Problem in einer Standardversion und/oder Beispieldaten auch auftritt.
+        ---
+        Vorlage zum kopieren 
+        ```
+        Version: 
+        Oberfläche: 
+        Datenbankformat:
+
+        Kurzbeschreibung: 
+        Beschreibung: 
+        ```
+        ---
+        Bitte verwenden Sie Bilder oder ein Video zusätzlich zur Beschreibung
+        des Problems, da diese helfen um das Problem zu verstehen.
+        """
+        bot_handler.send_reply(message, content)
+
+
+
+
+handler_class = HelloWorldHandler
---
+++ zulip_bots/zulip_bots/bots/Info/doc.md
@@ -0,0 +1,3 @@
+Simple Zulip bot that will respond to any query with a "not enough information" message.
+
+
