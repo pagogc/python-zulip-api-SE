866ca5fd137a8a199c729b86f2ab74dd89223386
Michael Pagler <michael.pagler@softengine.at>
2025-04-15 08:14:45 +0200

    debug and testflags

--- zulip_bots/zulip_bots/bots/redmine/redmine.py
+++ zulip_bots/zulip_bots/bots/redmine/redmine.py
@@ -12,7 +12,6 @@ import zulip
 from zulip_bots.lib import BotHandler
 
 
-GET_REGEX = re.compile('get "(?P<issue_key>.+)"$')
 CREATE_REGEX = re.compile(
     'create issue\s*(?P<remaining_text>[\s\S]*)'
     "$"
@@ -25,20 +24,6 @@ CREATE_REGEX2 = re.compile(
     'create\s*(?P<remaining_text>[\s\S]*)'
     "$"
 )
-EDIT_REGEX = re.compile(
-    'edit issue "(?P<issue_key>.+?)"'
-    '( to use summary "(?P<summary>.+?)")?'
-    '( to use project "(?P<project_key>.+?)")?'
-    '( to use type "(?P<type_name>.+?)")?'
-    '( to use description "(?P<description>.+?)")?'
-    '( by assigning to "(?P<assignee>.+?)")?'
-    '( to use priority "(?P<priority_name>.+?)")?'
-    '( by labeling "(?P<labels>.+?)")?'
-    '( by making due "(?P<due_date>.+?)")?'
-    "$"
-)
-SEARCH_REGEX = re.compile('search "(?P<search_term>.+)"$')
-JQL_REGEX = re.compile('jql "(?P<jql_query>.+)"$')
 HELP_REGEX = re.compile("help$")
 
 HELP_RESPONSE = """
@@ -58,7 +43,7 @@ Issue Bot:
 """
 
 
-class JiraHandler:
+class RedmineHandler:
     def usage(self) -> str:
         return """
         Erzeugt ein Issue für dieses Thema, wenn du mich erwähnst und den Befehl dazu gibst.
@@ -75,6 +60,12 @@ class JiraHandler:
         users_to_redirect= config.get("users_to_redirect")
         redirect_to_redmine_userID = config.get("redirect_userid")
         fallback_redmine_userID = config.get("fallback_userid")
+        debug = False
+        if config.get("debug"):
+            debug = True
+        testing= False
+        if config.get("testing"):
+            testing = True
 
         self.rcfile = config.get("zulip_rc_file")
         if not redmine_url:
@@ -92,50 +83,40 @@ class JiraHandler:
         self.redirect_userlist = users_to_redirect.split(',') 
         self.redirect_userID  = redirect_to_redmine_userID  
         self.fallback_userID = fallback_redmine_userID 
+        self.debug=debug 
+        self.testing = testing
         self.zulipclient = zulip.Client(config_file=self.rcfile)
         
 
-    def jql_search(self, jql_query: str) -> str:
-        unknown_val = "*unknown*"
-        jira_response = requests.get(
-            self.domain_with_protocol
-            + f"/rest/api/2/search?jql={jql_query}&fields=key,summary,status",
-            headers={"Authorization": self.auth},
-        ).json()
-
-        url = self.display_url + "/browse/"
-        errors = jira_response.get("errorMessages", [])
-        results = jira_response.get("total", 0)
-
-        if errors:
-            response = "Oh no! Jira raised an error:\n > " + ", ".join(errors)
-        else:
-            response = f"*Found {results} results*\n\n"
-            for issue in jira_response.get("issues", []):
-                fields = issue.get("fields", {})
-                summary = fields.get("summary", unknown_val)
-                status_name = fields.get("status", {}).get("name", unknown_val)
-                response += "\n - {}: [{}]({}) **[{}]**".format(
-                    issue["key"], summary, url + issue["key"], status_name
-                )
-
-        return response
-
     def handle_message(self, message: Dict[str, str], bot_handler: BotHandler) -> None:
 
-        client = self.zulipclient
         sender_id = message.get("sender_id")
+        content = message.get("content")
+        subject_from_Message = message.get("subject")
+        message_id = message.get("id")
+        message_type = message.get("type")
+
+        if self.testing:
+            logging.basicConfig(filename='debug.log', level=logging.INFO)
+
+        if self.testing:
+            logging.info("TESTING: Sender selbst gesetzt")
+            sender_id = 29 #Jens Simon
+
+        if self.debug:
+            logging.info("DEBUG: SenderID Zulip: {}", sender_id)
+
+        client = self.zulipclient
         resultuser = client.get_user_by_id(sender_id)
         result = resultuser.get("user")
 
-        content = message.get("content")
-        subject_from_Message = message.get("subject")
         mail_of_sender = result.get("delivery_email")
         if mail_of_sender is None:
             mail_of_sender = "nichtbekannt"
             
-        message_id = message.get("id")
-        message_type = message.get("type")
+        if self.debug:
+            logging.info("DEBUG: delivery_email Zulip: {}", mail_of_sender)
+
         project_name='themen-aus-teamzone'
         backup_user_id = self.fallback_userID
  
@@ -163,6 +144,11 @@ class JiraHandler:
             create_match = CREATE_REGEX2.match(content)
             
         help_match = HELP_REGEX.match(content)
+
+        if self.testing:
+            content = "@**Issubot** create\nTestMessage"
+            subject_from_Message = "DebugMessage"
+            message_id = 0
 
         issue_subject= "Thema von Teamzone: " + subject_from_Message
         issue_response = ""
@@ -196,7 +182,9 @@ class JiraHandler:
                 
                 #get all messages of Topic 
                 message_stream_id = message.get("stream_id")
-                message_subject = message.get("subject")
+                message_subject = subject_from_Message
+                if self.testing:
+                    message_stream_id = 1
                 message_subject.replace(" ", "+")
                 request: Dict[str, Any] = {
                     "apply_markdown": False, 
@@ -239,131 +227,4 @@ class JiraHandler:
 
         bot_handler.send_reply(message, response)
 
-
-def make_jira_auth(username: str, password: str) -> str:
-    """Makes an auth header for Jira in the form 'Basic: <encoded credentials>'.
-
-    Parameters:
-     - username: The Jira email address.
-     - password: The Jira password.
-    """
-    combo = username + ":" + password
-    encoded = base64.b64encode(combo.encode("utf-8")).decode("utf-8")
-    return "Basic " + encoded
-
-
-def make_create_json(
-    summary: str,
-    project_key: str,
-    type_name: str,
-    description: Optional[str],
-    assignee: Optional[str],
-    priority_name: Optional[str],
-    labels: Optional[str],
-    due_date: Optional[str],
-) -> Any:
-    """Makes a JSON string for the Jira REST API editing endpoint based on
-    fields that could be edited.
-
-    Parameters:
-     - summary: The Jira summary property.
-     - project_key: The Jira project key property.
-     - type_name (optional): The Jira type name property.
-     - description (optional): The Jira description property.
-     - assignee (optional): The Jira assignee property.
-     - priority_name (optional): The Jira priority name property.
-     - labels (optional): The Jira labels property, as a string of labels separated by
-                          comma-spaces.
-     - due_date (optional): The Jira due date property.
-    """
-    json_fields = {
-        "summary": summary,
-        "project": {"key": project_key},
-        "issuetype": {"name": type_name},
-    }
-    if description:
-        json_fields["description"] = description
-    if assignee:
-        json_fields["assignee"] = {"name": assignee}
-    if priority_name:
-        json_fields["priority"] = {"name": priority_name}
-    if labels:
-        json_fields["labels"] = labels.split(", ")
-    if due_date:
-        json_fields["duedate"] = due_date
-
-    json = {"fields": json_fields}
-
-    return json
-
-
-def make_edit_json(
-    summary: Optional[str],
-    project_key: Optional[str],
-    type_name: Optional[str],
-    description: Optional[str],
-    assignee: Optional[str],
-    priority_name: Optional[str],
-    labels: Optional[str],
-    due_date: Optional[str],
-) -> Any:
-    """Makes a JSON string for the Jira REST API editing endpoint based on
-    fields that could be edited.
-
-    Parameters:
-     - summary (optional): The Jira summary property.
-     - project_key (optional): The Jira project key property.
-     - type_name (optional): The Jira type name property.
-     - description (optional): The Jira description property.
-     - assignee (optional): The Jira assignee property.
-     - priority_name (optional): The Jira priority name property.
-     - labels (optional): The Jira labels property, as a string of labels separated by
-                          comma-spaces.
-     - due_date (optional): The Jira due date property.
-    """
-    json_fields = {}
-
-    if summary:
-        json_fields["summary"] = summary
-    if project_key:
-        json_fields["project"] = {"key": project_key}
-    if type_name:
-        json_fields["issuetype"] = {"name": type_name}
-    if description:
-        json_fields["description"] = description
-    if assignee:
-        json_fields["assignee"] = {"name": assignee}
-    if priority_name:
-        json_fields["priority"] = {"name": priority_name}
-    if labels:
-        json_fields["labels"] = labels.split(", ")
-    if due_date:
-        json_fields["duedate"] = due_date
-
-    json = {"fields": json_fields}
-
-    return json
-
-
-def check_is_editing_something(match: Any) -> bool:
-    """Checks if an editing match is actually going to do editing. It is
-    possible for an edit regex to match without doing any editing because each
-    editing field is optional. For example, 'edit issue "BOTS-13"' would pass
-    but wouldn't preform any actions.
-
-    Parameters:
-     - match: The regex match object.
-    """
-    return bool(
-        match.group("summary")
-        or match.group("project_key")
-        or match.group("type_name")
-        or match.group("description")
-        or match.group("assignee")
-        or match.group("priority_name")
-        or match.group("labels")
-        or match.group("due_date")
-    )
-
-
-handler_class = JiraHandler
+handler_class = RedmineHandler
